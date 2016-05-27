db8da66fa4fb4b98935371dc6aefb1b71c5716a3
pyromaticx <krginvest@gmail.com>
2016-05-27 11:45:32 -0700

    server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-app.use(express.static('style'));
-app.use(express.static('build'));
+app.use(express.static('./style'));
+app.use(express.static('./build'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
