640b3234be5a8ef39c47866bce7b518565105958
pyromaticx <krginvest@gmail.com>
2016-06-07 15:15:08 -0700

    ch ch changes

--- src/components/pdftemplate.js
+++ src/components/pdftemplate.js
@@ -43,7 +43,7 @@ export default function PDFTemplate(annotations, userData) {
    }
  }
 
-  var font = "<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>"
+  var font = "<link href='https://fonts.googleapis.com/css?family=Quicksand:700,400' rel='stylesheet' type='text/css'>"
   var coverPage = "<div style='width: 100%; height: 102%; display: -webkit-flex; -webkit-flex-direction: column; -webkit-justify-content: space-around; -webkit-align-items: center;'><h1 style='font-family: Quicksand;'>" + userData.collectionTitle + "</h1><h4 style='font-family: Quicksand;'>For: " + userData.companyName + "</h4><div style='font-family: Quicksand; text-align: center;'><h6 style='font-family: Quicksand;'>Prepared By:</h6><h3 style='font-family: Quicksand;'>Go Live Labs</h3></div><div style='font-family: Quicksand; text-align: center;'><h4>Contributers: </h4>" + contributers(annotations) + "</div></div>"
   var strings = annotations.map(function(annotation, idx) {
     //  return "<div style='width: 100%; height: 100%;'><div style='width: 100%; height: 50%; background-color: #f9f9f9;'><img style='position: relative; top: 15%; left: 2.5%; margin-top: 10px; margin-bottom: 10px;' src='" + annotation.annotationMedia + "' width='95%' /><div style='height: 40px; width: 100%; display: -webkit-flex; -webkit-justify-content: space-between; -webkit-align-items: center; background-color: " + colorByType(annotation.annotationType) + ";'><div style='width:500px; display: -webkit-flex; -webkit-justify-content: flex-start; -webkit-align-items: center;'><img style='position: relative; margin-left: 10px;' src='https://s3-us-west-1.amazonaws.com/uxpjpeg/svg/annotationWhiteB.svg' width='50px'><h3 style='color: #fff; font-family: Quicksand; font-weight: 700; margin-top: 30px; margin-left: 10px;'>" + annotation.annotationType + "</h3></div><div style='display: -webkit-flex; -webkit-justify-content: center; -webkit-align-items: center; font-family: Quicksand; color: #fff; width: 100%;'><h3>" + annotation.pinType + "</h3></div></div><div style='width: 100%; display: -webkit-flex; -webkit-justify-content: center; -webkit-align-items: center; -webkit-flex-direction: column; font-family: Quicksand; color: #333;'><h4>Title: " + annotation.annotationTitle + "<h5>Description: " + annotation.annotationText + "</h5><h5>Domain: <a target='_blank' href='" + annotation.specificURL + "'>" + annotation.specificURL + "</a></h5></div></div></div>"
@@ -55,10 +55,10 @@ export default function PDFTemplate(annotations, userData) {
             "<h5 style='font-family: Quicksand;'>" + moment(Date.now()).format('MM/DD/YYYY') + "</h5>" +
           "</div>" +
           "<div style='width: 100%; height: 99%; display: -webkit-flex; -webkit-justify-content: flex-start; -webkit-align-items: flex-start; -webkit-flex-direction: column;'>" +
-            "<p style='word-break: break-all; font-family: Quicksand; font-weight: bold; font-size: 15px;'>Annotation Title: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.annotationTitle + "</span></p>" +
-            "<p style='word-break: break-all; font-family: Quicksand; font-weight: bold;'>Annotation Description: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.annotationText + "</span></p>" +
-            "<p style='word-break: break-all; font-family: Quicksand; font-weight: bold;'>Annotation Type: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.pinType + "</span></p>" +
-            "<p style='word-break: break-all; font-family: Quicksand; font-weight: bold; margin-bottom: 10px;'>Annotation URL: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.specificURL + "</span></p>" +
+            "<p style='word-break: break-all; font-family: Quicksand; font-weight: 700; font-size: 15px;'>Annotation Title: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.annotationTitle + "</span></p>" +
+            "<p style='word-break: break-all; font-family: Quicksand; font-weight: 700;'>Annotation Description: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.annotationText + "</span></p>" +
+            "<p style='word-break: break-all; font-family: Quicksand; font-weight: 700;'>Annotation Type: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.pinType + "</span></p>" +
+            "<p style='word-break: break-all; font-family: Quicksand; font-weight: 700; margin-bottom: 10px;'>Annotation URL: <span style='font-family: Quicksand; font-weight: 400;'>" + annotation.specificURL + "</span></p>" +
             "<div style='width: 100%; display: -webkit-flex; -webkit-justify-content: center; -webkit-align-items: center;'>" +
               "<img style='' src='" + annotation.annotationMedia + "' height='225px' />" +
             "</div>" +
@@ -66,7 +66,7 @@ export default function PDFTemplate(annotations, userData) {
             "<div style='border-bottom: 1px solid #666; width: 100%; margin-bottom: 20px; margin-top: 20px;'><span style='font-family: Quicksand;'>Comments:</span></div>" +
             "<div style='border-bottom: 1px solid #666; width: 100%; margin-bottom: 20px;'></div>" +
             "<div style='border-bottom: 1px solid #666; width: 100%; margin-bottom: 20px;'></div>" +
-            "<div style='position: relative; height: 60px; width: 100%; top: -3px; border-top: 2px solid #333; box-sizing: border-box; display: -webkit-flex; -webkit-justify-content: space-between; -webkit-align-items: center; -webkit-flex-direction: row;'>" +
+            "<div style='position: relative; height: 60px; width: 100%; border-top: 2px solid #333; box-sizing: border-box; display: -webkit-flex; -webkit-justify-content: space-between; -webkit-align-items: center; -webkit-flex-direction: row;'>" +
               "<h5 style='font-family: Quicksand;'>Confidential</h5>" +
               "<h5 style='font-family: Quicksand;'>Prepared by Go Live Labs</h5>" +
               "<h5 style='font-family: Quicksand;'>Page: " + (idx+1) + " of " + annotations.length + "</h5>" +
