5cf05e99c5df31486d36efbed5b40ec38af6692d
pyromaticx <krginvest@gmail.com>
2016-07-13 14:03:14 -0700

    yaaa

--- src/components/annotation.js
+++ src/components/annotation.js
@@ -71,7 +71,7 @@ export default class Annotation extends Component {
         }
         case 'webcam': {
           return (
-            <div>
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center'}}>
               <video className={this.props.annotation.annotationId} src={this.props.annotation.annotationMedia} width='100%' />
               <video className={this.props.annotation.annotationId} src={this.props.annotation.userVideo} width='30%' />
               <button className='btn btn-default' style={{position: 'relative', left: '50%', transform: 'translateX(-50%)'}} type='button' onClick={(event) => {this.playBoth(event)}}>{this.state.playing ? 'Pause' : 'Play'}</button>
